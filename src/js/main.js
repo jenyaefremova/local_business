7ab16b8f8b93722715eeb2ceef49d08283c44954
jenyaefremova <jenaefremova@gmail.com>
2019-05-31 11:53:40 +0300

    доделано меню интерактив

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,6 @@ var menu = document.querySelector('#navbar-menu');
 button.addEventListener('click', function() {
   menu.classList.add("navbar-block__menu_active");
 });
+menu.addEventListener('mouseleave', function() {
+  menu.classList.remove("navbar-block__menu_active");
+});
